79c31e68489f6e4adf538ebdab79ed462bb75cd3
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-07-03 00:52:07 +0330

    init stride project

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  compatibilityDate: "2024-07-02",
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptatum
+      dicta sunt modi cumque, voluptate, numquam saepe officia unde ex aliquid
+      quas porro veniam nihil fugiat architecto laborum. Atque, repellendus?
+    </p>
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style scoped>
+/*  */
+</style>
